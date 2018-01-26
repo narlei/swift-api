b191d607c7c5c24ac90f35302c218f9c60a3fc52
Narlei Moreira <narlei.guitar@gmail.com>
2018-01-26 10:50:30 -0200

    a

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -4,7 +4,7 @@ extension Droplet {
     func setupRoutes() throws {
         get("hello") { req in
             var json = JSON()
-            try json.set("hello", "world")
+            try json.set("hello", "SocialBase aqui é swift!!!!")
             return json
         }
 
